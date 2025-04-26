e7d7233c8221236052e13d98e5fcefb8332c8a5d
Al-AminWeb <143194944+Al-AminWeb@users.noreply.github.com>
2025-04-26 23:43:58 +0600

    Integrate Github Authentication provider

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
 import {auth} from "../../fire/firebase_init.js";
 import {getAuth, signOut} from "firebase/auth";
+import { GithubAuthProvider } from "firebase/auth";
 
 const Login = () => {
     const [user, setUser] = useState(null);
 
     const provider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
 
     const handleGoogleSignIn = () => {
@@ -31,16 +33,31 @@ const Login = () => {
             .catch(error => {
                 console.log(error);
             })
-
-
     }
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
+            .then(result => {
+                console.log(result);
+                setUser(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
             <h2>Please Log in here </h2>
 
 
             {
-                user ? <button onClick={handleSignOut}>Sign Out</button>:<button onClick={handleGoogleSignIn}>Sign in with google</button>
+                user ? <button onClick={handleSignOut}>Sign Out</button> :
+                    <>
+                        <button onClick={handleGoogleSignIn}>Sign in with google</button>
+
+                        <button onClick={handleGithubSignIn}>Sign in wth github</button>
+
+                    </>
 
 
             }
